3e4c086272eeed53cab54f80f846b9b7e5e663ca
Sukhmeet <sukhbawa2049@gmail.com>
2022-01-02 13:52:04 +0530

    Working

---
+++ amdgpu-core-shim.spec
@@ -0,0 +1,10 @@
+Provides: amdgpu-core
+Summary: Workaround Package for amdgpu-core.
+Name: amdgpu-core-shim
+Version: 0
+Release: 0
+License: Public
+%description
+Workaround to package to skip installation of amdgpu-core as it is not required for Fedora.
+%files
+%clean	
---
+++ opencl-amd.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env sh
+set -e
+
+rootCheck()
+{
+    if [[ $UID -ne 0 ]]; then
+        sudo -p 'Restarting as Root, Password: ' bash $0 "$@"
+        exit $?
+    fi
+}
+
+installOpenCL()
+{
+    dnf install http://repo.radeon.com/amdgpu-install/latest/rhel/8.5/amdgpu-install-21.40.40500-1.noarch.rpm -y
+    sed -i 's/$amdgpudistro/8.5/g' /etc/yum.repos.d/amdgpu*.repo
+    dnf update
+    if  [ "$(dnf list installed | grep mesa-libOpenCL | wc -l)" == 1 ]; then
+        echo "Removing Mesa OpenCL"
+        dnf remove mesa-libOpenCL -y
+    fi
+    if  [ "$(dnf list installed | grep rpm-build.$(arch) | wc -l)" == 0 ]; then
+        dnf install rpm-build -y
+        remove=1
+    fi
+    rpmbuild -bb ./amdgpu-core-shim.spec --define "_rpmdir $(pwd)"
+    dnf install $(pwd)/$(arch)/amdgpu-core-shim*.rpm -y
+    dnf install rocm-opencl-runtime libdrm-amdgpu -y
+    if  [ remove == 1 ]; then
+        dnf remove rpm-build -y
+    fi
+}
+
+uninstallOpenCL()
+{
+    dnf remove rocm-opencl-runtime libdrm-amdgpu amdgpu-core-shim -y
+    dnf remove amdgpu-install -y
+}
+
+menu()
+{
+    PS3='Enter Option Number: '
+    options=("Install" "Uninstall" "Quit")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "Install")
+                echo "Installing OpenCL Stack"
+                installOpenCL
+                echo "Install Successful"
+                break
+                ;;
+            "Uninstall")
+                echo "Uninstalling OpenCL Stack"
+                uninstallOpenCL
+                echo "Uninstall Successfull"
+                break
+                ;;
+            "Quit")
+                break
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
+
+}
+
+#Driver Code
+rootCheck
+menu
